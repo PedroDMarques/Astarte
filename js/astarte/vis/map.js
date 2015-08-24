0764eeb620d90677b1b18fe0e315df6db53d213e
Pedro Marques <pedro.dalannar.marques@gmail.com>
2015-08-24 20:08:55 +0100

    Stuff
    Stuff

--- js/astarte/vis/map.js
+++ js/astarte/vis/map.js
@@ -1,3 +1,30 @@
 /*global astarte*/
 /*global L*/
 
+// API key to use mapbox tiles. Will be changed later to specify when initializing the map
+L.mapbox.accessToken = "pk.eyJ1IjoiZGFsYW5uYXIiLCJhIjoiYjA0MTcyZDMyNzg2YWNjYTA3ZGE1MGMxMDI5ZWMyYjgifQ.25SjWDdKObbZvLpuGwZM4A";
+
+
+astarte.Map = L.mapbox.map.extend({
+	
+	// -----------------------------------------------------------------
+	options: {
+		
+	},
+	
+	// -----------------------------------------------------------------
+	initialize: function(DOMid, mapId, options){
+		
+		L.mapbox.Map.prototype.initialize.call(this, DOMid, mapId, options);
+		
+	},
+	
+	// -----------------------------------------------------------------
+	setOptions: function(options){
+		L.setOptions(this, options);
+		return this;
+	},
+	
+	
+	
+});
