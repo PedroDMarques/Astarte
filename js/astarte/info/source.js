333b54a676dc8d434e855bac575f69c8f3ed3635
Pedro Marques <pedro.dalannar.marques@gmail.com>
2015-09-17 17:03:59 +0100

    Added to source.js's code

--- js/astarte/info/source.js
+++ js/astarte/info/source.js
@@ -14,7 +14,14 @@ astarte.Source = astarte.Class.extend({
 		
 	},
 	
-	// -----------------------------------------------------------------
+	/* -----------------------------------------------------------------
+	Constructor to create a new source object.
+	sourceVars = {
+		"deviceMac" : deviceMac,
+		"userType" : userType,
+	}
+	Initializes this._locations object to store information about all the locations this device has sent data from
+	----------------------------------------------------------------- */
 	initialize: function(sourceVars, objNet, options){
 		astarte.Class.prototype.initialize.call(this, objNet, options);
 		
@@ -100,6 +107,58 @@ astarte.Source = astarte.Class.extend({
 	},
 	
 	// -----------------------------------------------------------------
+	getLatestLocation: function(curTime, timeType, filter){
+		var toRet = null;
+		
+		// Make sure that type of time actually exists
+		if(this._locations.ordering[timeType]){
+			var found = false;
+			var i = this._locations.ordering[timeType].length - 1;
+			
+			while(!found){
+				if(i === -1){
+					toRet = null;
+					found = true;
+				}else{
+					var index = this._locations.ordering[timeType][i];
+					var obj = this._locations.objects[index];
+					
+					// Make sure the time is under curTime supplied
+					if(obj[timeType] <= curTime){
+						
+						// Make sure the information passes the filters test
+						if(filter.filter(obj)){
+							toRet = obj;
+							found = true;	
+						}
+					}
+				}
+				i--;
+			}
+		}
+		
+		return toRet;
+	},
+	
+	// -----------------------------------------------------------------
+	getUnorderableObjects: function(timeType, filter){
+		var toRet = [];
+		for(var i = 0; i < this._locations.unorderable[timeType].length; i++){
+			var index = this._locations.unorderable[timeType][i];
+			var obj = this._locations.objects[index];
+			if(filter.filter(obj)){
+				toRet.push(obj);
+			}
+		}
+		return toRet;
+	},
+	
+	// -----------------------------------------------------------------
+	getLocationsCount: function(){
+		return this._locations.objects.length;
+	},
+	
+	// -----------------------------------------------------------------
 	getDeviceMac: function(){
 		return this._deviceMac;
 	},
@@ -107,6 +166,26 @@ astarte.Source = astarte.Class.extend({
 	// -----------------------------------------------------------------
 	getUserType: function(){
 		return this._userType;
-	}
+	},
+	
+	// -----------------------------------------------------------------
+	getLocation: function(locationId){
+		return this._locations.objects[locationId];
+	},
+	
+	// -----------------------------------------------------------------
+	getLocations: function(){
+		return this._locations.objects;
+	},
+	
+	// -----------------------------------------------------------------
+	getOrderedLocations: function(timeType){
+		var toRet = [];
+		for(var i = 0; i < this._locations.ordering[timeType].length; i++){
+			var index = this._locations.ordering[timeType][i];
+			toRet.push(this._locations.objects[index]);
+		}
+		return toRet;
+	},
 	
 });
